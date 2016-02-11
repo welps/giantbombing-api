cb5d21a1aba7206aa32a139772cb77f2b08375d4
welps <waynethecheng@gmail.com>
2016-02-11 13:05:33 +0000

    More tests to build out validation

--- tests/lib/giantbomb.test.js
+++ tests/lib/giantbomb.test.js
@@ -7,7 +7,7 @@ var GiantBombFactory = function (config){
     return function(){
         return new giantbombapi(config);
     };
-}
+};
 
 test.before('Set up GiantBombAPI', it => {
     var mockConfig = {
@@ -17,7 +17,7 @@ test.before('Set up GiantBombAPI', it => {
             host: 'localhost',
             port: 6379
         }
-    }
+    };
 
     GiantBombAPI = GiantBombFactory(mockConfig)();
 });
@@ -33,5 +33,28 @@ test('throws error if not passed API key as a string', it => {
 
 test('has an api key', it => {
     it.same(GiantBombAPI.apiKey, '12345abc');
+});
+
+test('throws error if not passed cacheOptions as an object', it => {
+    var mockConfig = {
+        apiKey: '12345abc',
+        cacheOptions: 'Hello'
+    };
+
+    it.throws(GiantBombFactory(mockConfig), 'Cache options must be passed as an object');
+});
+
+test('throws error if useCache is not true/undef and passed cacheOptions', it => {
+    var mockConfig = {
+        apiKey: '1nocache',
+        cacheOptions: {
+            host: 'localhost',
+            port: 6379
+        }
+    }
+    it.throws(GiantBombFactory(mockConfig), 'useCache must be true if using cache options');
+
+    mockConfig.useCache = false;
+    it.throws(GiantBombFactory(mockConfig), 'useCache must be true if using cache options');
 });
 
