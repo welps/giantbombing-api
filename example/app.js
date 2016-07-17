8e55a10be7380a5187e01d4dbb1cfbd95279fdde
welps <waynethecheng@gmail.com>
2016-07-17 15:45:24 +0000

    Examples on how to use this API

---
+++ example/app.js
@@ -0,0 +1,58 @@
+// Examples on how to use the GiantBomb API Wrapper
+var GiantBombAPI = require('../lib/giantbomb');
+
+var config = {
+    apiKey: 'Your GiantBomb API code here'
+};
+
+var giantBombAPI = new GiantBombAPI(config);
+
+// Example 1 - We want to search through GiantBomb's catalog for Metroid Prime 3 and its game ID
+var options = {
+    field_list: 'name,id',
+    query: 'Metroid Prime 3: Corruption',
+    resources: 'game',
+    resource_type: 'game',
+    limit: 20
+};
+
+giantBombAPI.getSearch(options, function(err, res) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(res);
+});
+
+
+// Example 2 - We want the name, deck (a short desc), and platforms 'Metroid Prime 3' is on
+var options = {
+    field_list: 'name,deck,platforms',
+};
+
+giantBombAPI.getGame('4725', options, function(err, res) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(res);
+});
+
+
+// Example 3 - We want to find Unprofessional Fridays videos, sorted by publish date
+var options = {
+    sort: {
+        publish_date: 'desc'
+    },
+    filter: {
+        name: 'Unprofessional Fridays'
+    },
+    limit: 10
+}
+
+giantBombAPI.getVideos(options, function(err, res) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(res);
+});
+
+
